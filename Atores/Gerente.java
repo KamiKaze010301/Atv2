1f5529f854e04af859167f9d15da5bcd072ba1c7
Danilo Carneiro Teles <57774112+KamiKaze010301@users.noreply.github.com>
2020-12-24 21:11:34 -0300

    Delete Gerente.java

--- Atores/Gerente.java
+++
@@ -1,9 +0,0 @@
-package Atores;
-public abstract class Gerente extends Funcionario {
-	
-	public Gerente(String nome, int matricula, String login,String senha) {
-		super(nome,matricula,login,senha);
-	}
-	
-	
-}
